0f23ce0d128f8f1eea74eaa4981d1b221d040748
Fabian Beyerlein <dev@beyerleinf.de>
2019-10-23 16:27:20 +0200

    chore(): Added readme and tests

---
+++ README.md
@@ -0,0 +1,7 @@
+# nyc-typescript-coverage-issue
+
+This repo describes an issue with nyc coverage where files without tests don't show up in coverage at all and don't decrease the overall coverage.
+
+## How to use
+
+Run `npm install` and then `npm test`. Coverage is 100% but it shouldn't.
--- src/tested.spec.ts
+++ src/tested.spec.ts
@@ -5,4 +5,32 @@ describe('isMyTestedInterface', () => {
   it('should return false when arg is null', () => {
     expect(isMyTestedInterface(null)).to.be.false;
   });
+
+  it('should return false when arg is undefined', () => {
+    expect(isMyTestedInterface(undefined)).to.be.false;
+  });
+
+  it('should return false when prop is undefined', () => {
+    const arg = {
+      property: undefined,
+    };
+
+    expect(isMyTestedInterface(arg)).to.be.false;
+  });
+
+  it('should return false when prop is not a string', () => {
+    const arg = {
+      property: 1,
+    };
+
+    expect(isMyTestedInterface(arg)).to.be.false;
+  });
+
+  it('should return true when arg is correct', () => {
+    const arg = {
+      property: 'abc',
+    };
+
+    expect(isMyTestedInterface(arg)).to.be.true;
+  });
 });
